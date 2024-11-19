83e16b57fdd12c7ad11c7cd84339818d4cd545d5
asj <32415389+asj8000@users.noreply.github.com>
2024-11-20 02:14:19 +0900

    Initial commit

---
+++ src/config/orm.config.ts
@@ -0,0 +1,34 @@
+import { registerAs } from '@nestjs/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { SeederOptions } from 'typeorm-extension';
+
+import { CreateTask1654578859820 } from '../migrations/1654578859820-CreateTask';
+import { Task } from '../todo/task.entity';
+import TaskFactory from '../todo/task.factory';
+import TaskSeeder from '../todo/task.seeder';
+
+const mainOptions: DataSourceOptions = {
+  type: 'mysql',
+  url: process.env.DATABASE_URL,
+  synchronize: false,
+  migrations: [CreateTask1654578859820],
+  entities: [Task],
+};
+
+const testOptions: DataSourceOptions & SeederOptions = {
+  type: 'sqlite',
+  database: ':memory:',
+  synchronize: false,
+  migrationsRun: true,
+  entities: [Task],
+  migrations: [CreateTask1654578859820],
+  factories: [TaskFactory],
+  seeds: [TaskSeeder],
+};
+
+// used by CLI commands
+export const AppDataSource = new DataSource({ ...testOptions, ...mainOptions });
+
+export default registerAs('orm', () =>
+  process.env.NODE_ENV === 'test' ? testOptions : mainOptions,
+);
